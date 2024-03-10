ea8746bfe3c7a6f4a4ba35372efb3e39d0b0137a
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2024-03-10 20:22:03 +0530

    bitwise.cpp

---
+++ bitwise.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a = 4;
+  int b = 6;
+  cout<<"(AND)A&B"<<a&b<<endl;
+  cout<<"(OR)A|B"<<a|b<<endl;
+  cout<<"(NOT)~A"<<~a<<endl;
+  cout<<"(XOR)A^B"<<a^b<<endl;
+  return 0;
+}
