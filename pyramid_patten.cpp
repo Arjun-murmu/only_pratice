dfd54e1b2338a2dc0c101b288e13c6f7dc418109
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2024-09-13 01:27:58 +0530

    Update pyramid_patten.cpp

--- pyramid_patten.cpp
+++ pyramid_patten.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main(){
 int n = 4;
 for(int i = 0;i<=n;i++){
+        //Space print 0 to n-i-1
         for(int j = 0;j<=n-i-1;j++){
             cout<<" "<<" ";
         }
+        //Num1 print
         for(int j = 1;j<=i+1;j++){
             cout<<j<<" ";
         }
+        //Num 2 Print
         // for(int j = 0;j<=i-1;j++){
         for(int j = i;j>=1;j--){
             cout<<j<<" ";
